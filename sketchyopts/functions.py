d175f65e48e334a3ae9418d2666785309cb30d2b
leiweimu <45750781+leiweimu@users.noreply.github.com>
2024-02-19 14:00:31 -0800

    Update functions.py

--- sketchyopts/functions.py
+++ sketchyopts/functions.py
@@ -8,8 +8,9 @@ def very_important_function():
 def foo():
     print("All the newlines above me should be deleted!")
 
-
-if condition:
-    print("No newline above me!")
-
+def bar(): 
+    if True: 
+        
+        print("No newline above me!")
+        
     print("There is a newline above me, and that's OK!")
