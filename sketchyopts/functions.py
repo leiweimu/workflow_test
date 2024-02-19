ea219599cb95cc5dbc6f07d9625f27e97ded6ff9
leiweimu <45750781+leiweimu@users.noreply.github.com>
2024-02-19 13:53:33 -0800

    Update functions.py

--- sketchyopts/functions.py
+++ sketchyopts/functions.py
@@ -1,26 +1,15 @@
-import os
-
-def very_important_function(template: str, *variables, file: os.PathLike, engine: str, header: bool = True, debug: bool = False):
-    """Applies `variables` to the `template` and writes to `file`."""
-    with open(file, 'w') as f:
-      j = [1,
+def very_important_function():
+    j = [1,
        2,
        3
       ]
+    return j
 
 def foo():
-    
     print("All the newlines above me should be deleted!")
 
 
 if condition:
-
     print("No newline above me!")
 
     print("There is a newline above me, and that's OK!")
-
-
-class Point:
-
-    x: int
-    y: int
