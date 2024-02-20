2db66c5fe7ea1233f46ec49ebd80c27b2836a064
leiweimu <45750781+leiweimu@users.noreply.github.com>
2024-02-20 01:32:39 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -42,9 +42,9 @@ autodoc_default_options = {
 autosummary_generate = True
 
 apidoc_module_dir = '../sketchyopts/'
-apidoc_output_dir = '.api/'
+apidoc_output_dir = 'api'
 # # apidoc_excluded_paths = ['../tests']
-# apidoc_separate_modules = True
+apidoc_separate_modules = True
 
 # -- Options for HTML output -------------------------------------------------
 
