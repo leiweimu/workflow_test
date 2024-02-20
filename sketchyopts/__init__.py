c5342e99affb76df6c0a2e0e2dfa54b256d12701
leiweimu <45750781+leiweimu@users.noreply.github.com>
2024-02-20 12:22:39 -0800

    Update __init__.py

--- sketchyopts/__init__.py
+++ sketchyopts/__init__.py
@@ -1,1 +1,8 @@
+from sketchyopts import functions
 
+very_important_function = functions.very_important_function
+foo = functions.foo
+bar = functions.bar
+daily_average = functions.daily_average
+
+__all__ = ("very_important_function", "foo", "bar", "daily_average")
