b48fd6e330dc5e3e377759ad4f347288ed1690e3
leiweimu <45750781+leiweimu@users.noreply.github.com>
2024-02-21 00:56:55 -0800

    Update functions.py

--- sketchyopts/functions.py
+++ sketchyopts/functions.py
@@ -28,4 +28,18 @@ def bar():
     print("There is a newline above me, and that's OK!")
 
 def daily_average(temperatures: list[float]) -> float:
+    """Computes daily average. 
+
+    This computes $$\frac{1}{n} \sum_{i=1}^{n} t_i$$.
+
+    References: 
+        Zhuang et al, 2020: https://arxiv.org/abs/2010.07468
+
+    Args:
+        temperatures: list of daily temperatures
+
+    Returns:
+        average: average of daily temperature samples
+        
+    """
     return sum(temperatures) / len(temperatures)
